693c872be612f6d18b18cc8f5644ced7af03a20e
Madie Denman <madeline.denman@selu.edu>
2024-10-01 19:18:05 +0000

    added new information

--- README.R
+++ README.R
@@ -1,3 +1,6 @@
 # Sample R package 
 ## an example of an R package
 ### Author: Madie Denman
+
+#Learning how to add and commit something into github
+# to do this, first save this file and then click on this file in Git tab, finally, hit commit
