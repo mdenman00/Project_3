34ad5818f2cd80d635c26bb157927c5e87cf80ee
Madie Denman <madeline.denman@selu.edu>
2024-10-03 18:29:09 +0000

    trying github for real

--- README.R
+++ README.R
@@ -5,3 +5,5 @@
 #Learning how to add and commit something into github
 # to do this, first save this file and then click on this file in Git tab, finally, hit commit
 #something new 
+
+#adding something and making sure it makes it to github
