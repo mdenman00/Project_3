1c6586075dd1de9049e79473f143c58280196b84
Madie Denman <madeline.denman@selu.edu>
2024-10-31 19:15:19 +0000

    gutselli

--- R/gutselli_function.R
+++ R/gutselli_function.R
@@ -1,9 +1,17 @@
+#' Finds average and returns text if size is too small
+#'
+#' @param filepath The path to the tab-delimited file you want to process
+#' @param column The column in the data you want to find the average of
+#'
+#' @return "Too small" if average is less than it should be
+#' @export
 gutselli_function = function(filepath, column){
-  df = read_csv(filepath, na=c("", "-","NA"))
-  result = df %>%
+  df = readr::read_csv(filepath, na=c("", "-","NA"))
+  result = dplyr::df %>%
     pull({{column}}) %>%
     na.omit %>%
     mean
   if (result < 20){ print("Too Small")}
   else { return(result)}
 }
+
