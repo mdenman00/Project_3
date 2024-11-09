1f98869c78f589ce4fc17aa77712ca3885bb7a32
Madie Denman <madeline.denman@selu.edu>
2024-11-09 03:58:32 +0000

    update to plotting

--- R/plotting.R
+++ R/plotting.R
@@ -6,8 +6,12 @@
 #'
 #' @return
 #' @export
-plotting <- function(data, x, y) {
+plotting <- function(data, x, y, title = "title", x_lab = "x-lab", y_lab = "y-lab") {
   plot <- ggplot2::ggplot(data, aes(x = x, y = y)) +
-    geom_point +
-    theme_minimal
+    geom_point(alpha = .1, color = "hotpink") +
+    labs(title = title,
+         x = x_lab,
+         y = y_lab)
+  theme_minimal()
+  return(plot)
 }
