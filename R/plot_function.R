43739281f5f222331cb7f8dbdb82f461377494d7
Madie Denman <madeline.denman@selu.edu>
2024-11-25 19:36:34 +0000

    updated again

--- R/plot_function.R
+++ R/plot_function.R
@@ -3,8 +3,16 @@
 #' @param data the data you want to plot
 #' @param x the variable you want on the x-axis
 #' @param y the variable you want on the y-axis
+#' @param geom what kind of graph do you want (point, bar or line)
+#' @param title title of the graph
+#' @param x_lab x-axis label
+#' @param y_lab y-axis label
+#' @param color color you want on your graph
+#' @param alpha alpha size
+#' @param size size of graph
+#' @param theme what theme do you want
 #'
-#' @return
+#' @return returns graph that user created
 #' @export
 point_line_bar = function(data, x, y, geom = "point", title = "title", x_lab = "x-lab", y_lab = "y-lab",
                      color = "hotpink", alpha = 0.5, size = 2, theme = theme_minimal()) {
